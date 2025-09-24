67a366191c80b14beaf4e6e7b2045e1cf237b72a
jeywyson leandro <jeyads20@gmail.com>
2025-09-24 00:06:01 -0300

    finalizando 10 questoes basicas

--- Competição de Programação Python - Fundamentos/fundamentos.py
+++ Competição de Programação Python - Fundamentos/fundamentos.py
@@ -42,3 +42,62 @@ n4 = float(input("Digite sua quarta nota: "))
 media = (n1 + n2 + n3 + n4) / 4
 
 print(f"sua média é: {media}")
+
+# questao 5
+
+a = input("Digite o primeiro valor: ")
+b = input("Digite o segundo valor: ")
+
+a, b = b, a
+
+print("Valores invertidos: ")
+print(a)
+print(b)
+
+# questao 6
+
+C = float(input("Capital inicial: "))
+i = float(input("Taxa de juros (%) ")) / 100
+t = float(input("Tempo  (meses): "))
+
+print(f"Juros: {C * i * t:.2f}")
+print(f"Montante final: {C * (1 + i * t):.2f}")
+
+# questao 7
+
+distancia = float(input("Distância percorrida (km): "))
+litros = float(input("Combustível gasto (litros): "))
+try:
+    consumo = distancia / litros
+    saida = f"Consumo médio: {consumo:.2f} km/l"
+except ZeroDivisionError:
+    saida = "Não é possível calcular consumo: combustível gasto é 0"
+print(saida)
+
+# Questão 8
+
+anos = int(input("Anos: "))
+meses = int(input("Meses: "))
+dias = int(input("Dias: "))
+total_dias = anos * 365 + meses * 30 + dias
+print(f"Idade total em dias: {total_dias} dias")
+
+# Questão 9 
+
+valor_original = float(input("Valor original da compra: R$ "))
+percent = float(input("Percentual de desconto (%): "))
+desconto = valor_original * (percent / 100)
+valor_final = valor_original - desconto
+print(f"Valor do desconto: R$ {desconto:.2f}")
+print(f"Valor final: R$ {valor_final:.2f}")
+
+# Questão 10 
+
+distancia = float(input("Distância percorrida (km): "))
+tempo = float(input("Tempo gasto (horas): "))
+try:
+    velocidade_media = distancia / tempo
+    saida = f"Velocidade média: {velocidade_media:.2f} km/h"
+except ZeroDivisionError:
+    saida = "Não é possível calcular velocidade média: tempo = 0"
+print(saida)
