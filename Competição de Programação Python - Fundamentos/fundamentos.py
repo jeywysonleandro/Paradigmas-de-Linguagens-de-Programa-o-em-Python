7d6c9c9f295b321a0d0e3741e766613370f6fa68
jeywyson leandro <jeyads20@gmail.com>
2025-09-23 23:28:02 -0300

    Criando segunda questao

--- Competição de Programação Python - Fundamentos/fundamentos.py
+++ Competição de Programação Python - Fundamentos/fundamentos.py
@@ -11,4 +11,16 @@ divisao = num1 / num2
 print(f"Soma: {soma}")
 print(f"subtracao: {subtracao}")
 print(f"multiplicacao: {multiplicacao}")
-divisao(f"divisao: {divisao}")
+(f"divisao: {divisao}")
+
+# questao 2
+
+c = float(input("Digite a temperatura em Celsius: "))
+f = c * 9/5 + 32
+k = c + 273.15
+
+print(f"Fahrenheit: {f}")
+print(f"Kelvin: {k}")
+
+# questao 3
+
