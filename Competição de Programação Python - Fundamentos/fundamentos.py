94f8fc255a7f361d260c21fa8882feec351a01e2
jeywyson leandro <jeyads20@gmail.com>
2025-09-23 23:16:02 -0300

    Criando primeira questao

---
+++ Competição de Programação Python - Fundamentos/fundamentos.py
@@ -0,0 +1,14 @@
+# questao 1
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"Soma: {soma}")
+print(f"subtracao: {subtracao}")
+print(f"multiplicacao: {multiplicacao}")
+divisao(f"divisao: {divisao}")
