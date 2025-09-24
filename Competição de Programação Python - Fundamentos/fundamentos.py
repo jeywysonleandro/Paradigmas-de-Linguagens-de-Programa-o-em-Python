2ecd74338285fcc32ac5d89ac0abf8b3c1c6035e
jeywyson leandro <jeyads20@gmail.com>
2025-09-23 23:45:05 -0300

    quarta questao

--- Competição de Programação Python - Fundamentos/fundamentos.py
+++ Competição de Programação Python - Fundamentos/fundamentos.py
@@ -31,3 +31,14 @@ perimetro = 2 * (base + altura)
 
 print(f"Área: {area:.2f}")
 print(f"Perimetro: {perimetro:.2f}")
+
+# questao 4
+
+n1 = float(input("Digite sua primeira nota: "))
+n2 = float(input("Digite sua segunda nota: "))
+n3 = float(input("Digite sua terceira nota: "))
+n4 = float(input("Digite sua quarta nota: "))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print(f"sua média é: {media}")
